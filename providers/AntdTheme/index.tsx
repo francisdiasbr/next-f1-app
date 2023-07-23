af4788b2670dba11b24b0a1c6bfb7a16f7b20ef8
Francis Dias <francisdiasbr@gmail.com>
2023-07-23 18:26:48 -0300

    Configura AntdThemeProvider e ThemeProvider

---
+++ providers/AntdTheme/index.tsx
@@ -0,0 +1,31 @@
+import { ConfigProvider, ThemeConfig } from 'antd'
+import React from 'react'
+
+import { F1AppBrand } from '../../brand/index'
+
+const AntDesignConfigurationProvider = (props) => {
+
+  const { colors } = F1AppBrand
+
+  const antdConfig: ThemeConfig = {
+    token: {
+      'colorPrimary': colors.blue500,
+      'colorBorder': colors.blue300,
+
+      'blue-1': colors.blue100,
+      'blue-2': colors.blue200,
+      'blue-3': colors.blue300,
+      'blue-4': colors.blue400,
+      'blue-5': colors.blue500,
+      'blue-6': colors.blue600,
+    }
+  }
+
+  return (
+    <ConfigProvider theme={antdConfig}>
+      {props.children}
+    </ConfigProvider>
+  )
+}
+
+export default AntDesignConfigurationProvider
---
+++ providers/Theme/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { F1AppBrand } from 'brand'
+
+const ApplicationThemeProvider = (props) => {
+  return (
+    <ThemeProvider theme={F1AppBrand}>
+      {props.children}
+    </ThemeProvider>
+  )
+}
+
+export default ApplicationThemeProvider
