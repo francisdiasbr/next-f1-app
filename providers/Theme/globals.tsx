16822771eecb74058c3da5a1d7cea49e035c6649
Francis Dias <francisdiasbr@gmail.com>
2023-07-23 18:31:32 -0300

    Cria o componente GlobalStyles

---
+++ providers/Theme/globals.tsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: ${props => props.theme.colors.neutral600};
+    height: 100vh;
+    margin: 0px;
+  }
+
+  #__next {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    height: 100%;
+  }
+`
+
+export default GlobalStyles
