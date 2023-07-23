264fd9cde7c49de49d744c911ae5c3031bf52628
Francis Dias <francisdiasbr@gmail.com>
2023-07-23 18:35:43 -0300

    Cria o componente Container

---
+++ components/Container/component.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import * as S from './styles'
+
+export const Container = ({ header, content, footer }) => {
+  return (
+    <>
+      <S.Header>
+        {header}
+      </S.Header>
+      <S.Main>
+        <S.Section>
+          {content}
+        </S.Section>
+      </S.Main>
+      <S.Footer>
+        {footer}
+      </S.Footer>
+    </>
+  )
+}
---
+++ components/Container/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`
+
+export const Section = styled.section`
+  height: 100%;
+  width: 100%;
+`
+
+export const Footer = styled.footer`
+  width: 100%;
+`
