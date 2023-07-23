1f2dd4e151c006902f879dbb21adf1d60b082b59
Francis Dias <francisdiasbr@gmail.com>
2023-07-23 18:44:09 -0300

    Implementa Container e ClickableAsset no PageIndex

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,46 @@
-import AntdThemeProvider from 'providers/AntdTheme'
-import ThemeProvider from 'providers/Theme'
+import ClickableAsset from 'components/ClickableAsset'
+import PageContainer from 'components/Container'
+import { GetServerSideProps } from 'next'
 import React from 'react'
 
 const PageIndex = ({}) => {
+
+  const renderHeader = () => {
+    return (
+      <ClickableAsset
+        alt='teste'
+        href='/'
+        src='assets/logos/mainlogo.svg'
+        width='160px'
+      />
+    )
+  }
+
+  const renderContent = () => {
+    return (
+      <pre>content</pre>
+    )
+  }
+
+  const renderFooter = () => {
+    return (
+      <pre>footer</pre>
+    )
+  }
+
   return (
-    <ThemeProvider>
-      <AntdThemeProvider>
-        <pre>index page</pre>
-      </AntdThemeProvider>
-    </ThemeProvider>
+    <PageContainer
+      header={renderHeader()}
+      content={renderContent()}
+      footer={renderFooter()}
+    />
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    props: {}
+  }
 }
 
 export default PageIndex
